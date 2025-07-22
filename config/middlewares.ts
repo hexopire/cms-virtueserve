377d1360b8e4c35a2a92ed63e6301a879e9f4a94
KASULADEVI VINAY <2210030157@klh.edu.in>
2025-07-22 12:54:38 +0530

    Initial commit of Strapi CMS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // your frontend origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
